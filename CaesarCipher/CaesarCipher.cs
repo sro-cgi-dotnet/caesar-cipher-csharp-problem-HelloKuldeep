b2c679544ad81eecbc77b0ddf9c77b338f70eee6
Kuldeep Sharma <kul123sharma@gmail.com>
2018-09-17 09:38:13 +0530

    Solution-Caesar-@hellokuldeep-1.1

--- CaesarCipher/CaesarCipher.cs
+++ CaesarCipher/CaesarCipher.cs
@@ -1,13 +1,35 @@
 ﻿using System;
 using System.Linq;
 
-namespace CaesarCipher
-{
-    public static class RotationalCipher
-    {
-        public static string Rotate(string text, int shiftKey)
-        {
-            throw new NotImplementedException("You need to implement this function.");
+namespace CaesarCipher{
+    public static class RotationalCipher{
+        public static string Rotate(string text, int shiftKey){
+            char[] buffer = text.ToCharArray();
+            for (int i = 0; i < buffer.Length; i++){
+                char letter = buffer[i];
+                if(Char.IsLetter(buffer[i])){
+                    letter = (char)(letter + shiftKey); //Shift only if buffer is Character
+                } else {
+                    continue;
+                } 
+                if (Char.IsLower(buffer[i])){
+                    if (letter > 'z'){
+                        letter = (char)(letter - 26);   //Subtract 26 on overflow.
+                    }
+                    else if (letter < 'a'){
+                        letter = (char)(letter + 26);   //Add 26 on underflow.
+                    }
+                } else {
+                    if (letter > 'Z'){
+                        letter = (char)(letter - 26);
+                    }
+                    else if (letter < 'A'){
+                        letter = (char)(letter + 26);
+                    }
+                }
+                buffer[i] = letter;
+            }
+            return new string(buffer);
         }
     }
 }
